c0e9558cbfaed665091468d9c3e6dd4327078b6a
Martin Achtermann <mdacht777@gmail.com>
2020-06-14 00:46:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
     puts holiday_name
-    holiday_hash[:fall].push(columbus_day)
+    holiday_hash[:fall][:columbus_day]=supply_array
 holiday_hash
 end
 
